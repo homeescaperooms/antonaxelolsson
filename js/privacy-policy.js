f29cf36b441770c05005ddab74bb8a0e8779d5a4
Tony Spegel <TonySpegel@googlemail.com>
2020-05-15 17:33:42 +0200

    fix(privacy-plicy): add class to another div

--- js/privacy-policy.js
+++ js/privacy-policy.js
@@ -1,8 +1,10 @@
 window.onload = ()=> {
+    const grid = document.querySelector('#grid');
     const wrapperPrivacyPolicy = document.querySelector('#wrapper-privacy-policy');
     const checkboxPrivacyPolicy = document.querySelector('#checkbox-privacy-policy');
     
     checkboxPrivacyPolicy.addEventListener('click', () => {
+        grid.classList.add('fade-out');
         wrapperPrivacyPolicy.classList.add('fade-out');
         window.sntchChat.Init(52517);
     });
